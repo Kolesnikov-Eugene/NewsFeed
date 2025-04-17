49f227fac28378664bc0dbb6b49f49c6f0c2e2dd
Eugene Kolesnikov <eugene-kolesnikov@hotmail.com>
2025-04-17 15:15:21 +0300

    Initial

---
+++ NewsFeed/NewsFeed/NewsFeedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFeedViewController.swift
+//  NewsFeed
+//
+//  Created by Eugene Kolesnikov on 17.04.2025.
+//
+
+import UIKit
+
+final class NewsFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
